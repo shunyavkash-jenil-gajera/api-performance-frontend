879815d658e09e30d2a26320b8585dafc2789589
jenilGajera <jenil.gajera@shunyavkash.com>
2026-02-17 16:05:03 +0530

    add cridinles on env file

--- src/services/apiTesterService.js
+++ src/services/apiTesterService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://api-performance-backend.onrender.com";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export async function testApiRequest(payload) {
   const { data } = await axios.post(`${API_BASE_URL}/test`, payload, {
